fec447dbca95bb67d33e90ee165293a993e4638a
MikMac <miklosam1999@gmail.com>
2023-09-06 12:44:30 -0500

    finished traffic lights

---
+++ Assignments/P161/README.md
@@ -0,0 +1,25 @@
+## A05 - Traffic Lights
+### Miklos Moreno
+### Description:
+
+This is a solution to the "Traffic Lights" UVA problem on https://onlinejudge.org/. Problem ID 161.
+
+### Files
+
+|   #   | File                 | Description                                             |
+| :---: | -------------------- | ------------------------------------------------------- |
+|   1   | [161](161.pdf)       | Problem statement and guidelines                        |
+|   2   | [main](main.cpp)     | Main driver of my program which solves Hashmat problem. |
+|   3   | [data](data.txt)     | Sample input for program                                |
+|   4   | [uDebug](uDebug.txt) | input from uDebug                                       |
+|   5   | [out](out.txt)       | output for given data                                   |
+|   6   | [out2](out2.txt)     | output for uDebug data                                  |
+
+### Instructions
+
+- Just compile main.cpp, then run the executable from the command line but be sure to redirect
+input and output to the correct files.
+
+- Example Command:
+    - `g++ main.cpp -o main`
+    - `./main < <inputfile> > <outputfile>`
---
+++ Assignments/P161/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    int cTime, elapTime = 0;
+    vector<int> times;
+    bool Green = false;
+
+    // Cycle time must exist and != 0
+    while (cin >> cTime && cTime)
+    {
+        times.push_back(cTime);
+        // Stop reading in cycle times when 0 is entered
+        while (cTime)
+        {
+            times.push_back(cTime);
+            cin >> cTime;
+        }
+
+        // Start at the lowest cycle time, then double it to find the next time
+        // all lights will be green
+        elapTime = *min_element(times.begin(), times.end()) * 2;
+
+        do
+        {
+            Green = true;
+            for (int tempTime : times)
+            {
+                if (!(elapTime % (tempTime * 2) < (tempTime - 5)))
+                {
+                    Green = false;
+                    break;
+                }
+            }
+            // Test if lights are all green until 5 hours have passed
+        } while (!(Green) && ++elapTime <= 18000);
+
+        // If all lights are green, print the time
+        if (Green)
+        {
+            int hour = elapTime / 3600;
+            int min = (elapTime % 3600) / 60;
+            int sec = (elapTime % 3600) % 60;
+            cout << setfill('0') << setw(2) << hour << ':'
+                 << setw(2) << min << ':' << setw(2) << sec << endl;
+        }
+        else
+        {
+            cout << "Signals fail to synchronise in 5 hours" << endl;
+        }
+
+        // reset variables for next scenario
+        times.clear();
+        Green = false;
+    }
+
+    return 0;
+}
