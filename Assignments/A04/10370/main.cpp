bedcb16083e35cd2bfca88fa06040687ca84d9c5
MikMac <miklosam1999@gmail.com>
2023-11-08 08:25:52 -0600

    breezy

---
+++ Assignments/A04/10370/main.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int T, n;
+    int A[1000];
+
+    scanf("%d", &T);
+    
+    while (T--)
+    {
+        scanf("%d", &n);
+        double sum = 0;
+        int i, count = 0;
+        for (i = 0; i < n; i++)
+            scanf("%d", &A[i]), sum += A[i];
+        sum /= n;
+        for (i = 0; i < n; i++)
+            if (A[i] > sum)
+                count++;
+        printf("%.3lf%%\n", (double)count * 100 / n);
+    }
+    return 0;
+}
